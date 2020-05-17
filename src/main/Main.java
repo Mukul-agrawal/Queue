dd30d5c06b258f89c72b42f97bff5894ef210dd6
Mukul-agrawal <mukul.agrawal_cs18@gla.ac.in>
2020-05-17 22:36:27 +0530

    final commit

--- src/main/Main.java
+++ src/main/Main.java
@@ -9,6 +9,39 @@ public class Main {
         MyQueue<Integer> queue = new MyQueue<>();
         final Scanner scanner = new Scanner(System.in);
         int choice = 0;
-
+        while (true) {
+            System.out.println("Press 1 to \"add\" a node at the top.");
+            System.out.println("Press 2 to \"Poll\" a node from the top.");
+            System.out.println("Press 3 to \"Peek\" at the node at the top.");
+            System.out.println("Press 4 to \"remove\" a node from the top.");
+            System.out.println("Press 5 to \"element\" at the node at the top.");
+            System.out.println("Press 6 to exit.");
+            choice = scanner.nextInt();
+            switch (choice) {
+                case 1:
+                    System.out.println("Enter the data(int num) of the node to push: ");
+                    int data = scanner.nextInt();
+                    queue.offer(data);
+                    break;
+                case 2:
+                    int poppedData = queue.poll();
+                    break;
+                case 3:
+                    System.out.println("Top of the node: " + queue.peek());
+                    break;
+                case 4:
+                    int removeData = queue.poll();
+                    System.out.println("Remove : " + removeData);
+                    break;
+                case 5:
+                    System.out.println("element of the node: " + queue.element());
+                    break;
+                case 6:
+                    System.exit(0);
+                default:
+                    System.out.println("Please enter a valid choice!");
+            }
+        }
     }
 }
+
