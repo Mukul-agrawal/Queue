ab40acb83b734c1eb72a59c32633b058441bd286
Mukul-agrawal <mukul.agrawal_cs18@gla.ac.in>
2020-05-17 22:28:02 +0530

    create Scanner class object

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,9 +2,12 @@ package main;
 
 import defination.MyQueue;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         MyQueue<Integer> queue = new MyQueue<>();
+        final Scanner scanner = new Scanner(System.in);
 
     }
 }
